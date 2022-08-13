26de111e0825d948f0a11015a238520134d604e0
Anjo Tadena <tadena.anjo@gmail.com>
2022-08-13 10:05:11 +0800

    Setup react-navigation

--- App.tsx
+++ App.tsx
@@ -1,21 +1,33 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
 import {SafeAreaView, ScrollView, StatusBar, Text, View} from 'react-native';
 import {Button, Provider} from 'react-native-paper';
 
 import theme from './src/core/theme';
 
+const Stack = createStackNavigator();
+
+const Screen1 = () => (
+  <SafeAreaView>
+    <StatusBar />
+    <ScrollView contentInsetAdjustmentBehavior="automatic">
+      <View>
+        <Text>Hello, World!</Text>
+        <Button>Click Me!</Button>
+      </View>
+    </ScrollView>
+  </SafeAreaView>
+);
+
 const App = () => {
   return (
     <Provider theme={theme}>
-      <SafeAreaView>
-        <StatusBar />
-        <ScrollView contentInsetAdjustmentBehavior="automatic">
-          <View>
-            <Text>Hello, World!</Text>
-            <Button>Click Me!</Button>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="My App" component={Screen1} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 };
