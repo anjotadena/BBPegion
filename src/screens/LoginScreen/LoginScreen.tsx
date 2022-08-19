9a06e39571fb851ec3aa03b9f239c3e5e0c38ae6
Anjo Tadena <tadena.anjo@gmail.com>
2022-08-19 23:53:41 +0800

    Fix login form

--- src/screens/LoginScreen/LoginScreen.tsx
+++ src/screens/LoginScreen/LoginScreen.tsx
@@ -1,17 +1,13 @@
 import {StackNavigationProp} from '@react-navigation/stack';
-import React, {useState} from 'react';
-import {Alert, Text, TouchableOpacity, View} from 'react-native';
+import React from 'react';
+import {Text, TouchableOpacity, View} from 'react-native';
 
-import {loginUser} from '../../api/auth';
 import {Background, Logo} from '../../components';
 import BackButton from '../../components/BackButton/BackButton';
-import Button from '../../components/Button';
 import GoogleLogin from '../../components/GoogleLogin/GoogleLogin';
 import Header from '../../components/Header';
-import TextInput from '../../components/TextInput';
 import {RootStackParamList} from '../../types/app';
-import emailValidator from '../../utils/emailValidator';
-import passwordValidator from '../../utils/passwordValidator';
+import {LoginForm} from './components';
 import styles from './style';
 
 type ProfileScreenNavigationProp = StackNavigationProp<
@@ -24,78 +20,13 @@ type Props = {
 };
 
 const RegisterScreen = ({navigation}: Props): React.ReactElement => {
-  const [email, setEmail] = useState({value: '', error: ''});
-  const [password, setPassword] = useState({value: '', error: ''});
-  const [loading, setLoading] = useState(false);
-
-  const handleOnLoginPressed = async () => {
-    console.log({email, password});
-    const emailError = emailValidator(email.value);
-
-    if (emailError) {
-      setEmail({...email, error: emailError});
-    }
-
-    const passwordError = passwordValidator(password.value);
-
-    if (passwordError) {
-      setPassword({...password, error: passwordError});
-    }
-
-    if (emailError || passwordError) {
-      return;
-    }
-    setLoading(true);
-    const response = await loginUser({
-      email: email.value,
-      password: password.value,
-    });
-
-    setLoading(false);
-    if (response?.error) {
-      Alert.alert('Error', response.error);
-    } else {
-      setEmail({value: '', error: ''});
-      setPassword({value: '', error: ''});
-    }
-  };
-
   return (
     <Background>
       <BackButton goBack={navigation.goBack} />
       <Logo />
       <Header>Welcome</Header>
-      <TextInput
-        label="Email"
-        value={email.value}
-        error={!!email.error}
-        errorText={email.error}
-        onChangeText={text => setEmail({value: text, error: ''})}
-      />
-      <TextInput
-        secureTextEntry
-        label="Password"
-        value={password.value}
-        error={!!password.error}
-        errorText={password.error}
-        onChangeText={text => setPassword({value: text, error: ''})}
-      />
-      <View style={styles.row}>
-        <TouchableOpacity
-          onPress={() => navigation.navigate('ResetPasswordScreen')}>
-          <Text style={styles.link}>Forgot your password?</Text>
-        </TouchableOpacity>
-      </View>
-      <Button
-        mode="contained"
-        loading={loading}
-        disabled={loading}
-        onPress={handleOnLoginPressed}>
-        Login
-      </Button>
-
+      <LoginForm onLogin={() => {}} />
       <GoogleLogin />
-
       <View style={styles.row}>
         <Text>Don't have an account?</Text>
         <TouchableOpacity onPress={() => navigation.navigate('RegisterScreen')}>
---
+++ src/screens/LoginScreen/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,90 @@
+import {useNavigation} from '@react-navigation/native';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {useFormik} from 'formik';
+import React, {useState} from 'react';
+import {Alert, TouchableOpacity, View} from 'react-native';
+import {Button, Text} from 'react-native-paper';
+import * as yup from 'yup';
+import {loginUser} from '../../../../api/auth';
+
+import TextInput from '../../../../components/TextInput';
+import {RootStackParamList} from '../../../../types/app';
+import styles from './styles';
+
+const validationSchema = yup.object({
+  email: yup
+    .string()
+    .email('Enter a valid email')
+    .required('Email is required'),
+  password: yup
+    .string()
+    .min(8, 'Password should be of minimum 8 characters length')
+    .required('Password is required'),
+});
+
+const FORM_INITIAL_VALUES: {email: string; password: string} = {
+  email: '',
+  password: '',
+};
+
+type ProfileScreenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  'AuthLoadingScreen'
+>;
+
+type Props = {
+  onLogin: () => void;
+};
+
+const LoginForm = ({}: Props): React.ReactElement => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const navigation = useNavigation<ProfileScreenNavigationProp>();
+  const {touched, handleChange, isValid, errors, handleSubmit} = useFormik({
+    validationSchema,
+    initialValues: FORM_INITIAL_VALUES,
+    onSubmit: async values => {
+      setLoading(true);
+
+      try {
+        await loginUser(values);
+      } catch (e) {
+        Alert.alert(e?.message);
+      } finally {
+        setLoading(false);
+      }
+    },
+  });
+
+  return (
+    <>
+      <TextInput
+        label="Emailx"
+        error={touched.email && !!errors.email}
+        errorText={errors.email}
+        onChangeText={handleChange('email')}
+      />
+      <TextInput
+        secureTextEntry
+        label="Password"
+        error={touched.password && !!errors.password}
+        errorText={errors.password}
+        onChangeText={handleChange('password')}
+      />
+      <View style={styles.row}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('ResetPasswordScreen')}>
+          <Text style={styles.link}>Forgot your password?</Text>
+        </TouchableOpacity>
+      </View>
+      <Button
+        mode="contained"
+        loading={loading}
+        disabled={!isValid}
+        onPress={handleSubmit}>
+        Login
+      </Button>
+    </>
+  );
+};
+
+export default LoginForm;
